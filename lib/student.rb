384127898b0465efbfdb3e686a6c405a8d7e6796
Godwin <godwin.yampoi@student.moringaschool.com>
2023-06-01 22:59:19 +0300

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+
+    class Student < User
+
+        attr_accessor :knowledge
+      
+        def initialize 
+          super
+          @knowledge=[]
+        end
+      
+        def learn(addst)
+          @knowledge = addst
+        end
 
 end
